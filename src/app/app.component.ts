690643d8c16a46aaf7b27b511a6b83b1bb4097cc
muhndOlimy <muhnd.olimy@gmail.com>
2024-09-13 23:59:56 +0300

    Add navbar to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/common/search-bar/header.component';
+import { NavbarComponent } from "./components/common/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
