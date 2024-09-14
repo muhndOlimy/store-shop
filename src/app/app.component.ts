d34e58898d1d8b273c52f361a995272aef9e07ab
muhndOlimy <muhnd.olimy@gmail.com>
2024-09-14 10:34:36 +0300

    Add breadcrumb to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/common/search-bar/header.component';
 import { NavbarComponent } from "./components/common/navbar/navbar.component";
+import { BreadcrumbComponent } from './components/common/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, BreadcrumbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
