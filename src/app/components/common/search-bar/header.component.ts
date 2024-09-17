337592b3a3afc28a599df451a9f8ec36b44879c9
muhndOlimy <muhnd.olimy@gmail.com>
2024-09-17 20:17:34 +0300

    Add search event into header component

--- src/app/components/common/search-bar/header.component.ts
+++ src/app/components/common/search-bar/header.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
+import { loadProducts } from '../../../store/products/products.actions';
 
 @Component({
   selector: 'app-header',
@@ -12,4 +14,9 @@ import { InputTextModule } from 'primeng/inputtext';
 })
 export class HeaderComponent {
 
+  private _store = inject(Store);
+
+  search(event:any):void{
+    this._store.dispatch(loadProducts({skip:0 , limit:20 , query:event.target.value}));
+  }
 }
