334769eee9e739e98235a1bf3adce225a513e43e
muhndOlimy <muhnd.olimy@gmail.com>
2024-09-17 20:26:37 +0300

    Add pattern to validate search input

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -4,19 +4,28 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { loadProducts } from '../../../store/products/products.actions';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [IconFieldModule,InputIconModule,InputTextModule],
+  imports: [IconFieldModule,InputIconModule,InputTextModule,NgClass],
   templateUrl: './header.component..html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   private _store = inject(Store);
+  isInvalidSearch:boolean = false;
 
   search(event:any):void{
-    this._store.dispatch(loadProducts({skip:0 , limit:20 , query:event.target.value}));
+    const regex = /^[a-zA-Z0-9\s.,!?-]+$/;
+    if (regex.test(event.target.value)) {
+      this.isInvalidSearch = false;
+      this._store.dispatch(loadProducts({skip:0 , limit:20 , query:event.target.value}));
+    }else{
+      this.isInvalidSearch = true;
+    }
+
   }
 }
