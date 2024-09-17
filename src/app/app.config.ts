44877d65f74b66baaf41ba80b83ef6feebdb59e2
muhndOlimy <muhnd.olimy@gmail.com>
2024-09-17 18:34:52 +0300

    Register products reducers and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,16 +6,22 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { cartReducer } from './store/cart/cart.reducers';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideEffects } from '@ngrx/effects';
+import { ProductsEffects } from './store/products/products.effects';
+import { productDataReducer } from './store/products/products.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes, 
-    withComponentInputBinding()), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
     importProvidersFrom(BrowserAnimationsModule),
     provideStore({
-      cart:cartReducer
-    })
-  ]
+        cart: cartReducer,
+        productsData:productDataReducer
+    }),
+    provideEffects([
+      ProductsEffects
+    ])
+]
 };
